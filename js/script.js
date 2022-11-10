020d733de93a4a3f739e8324c98eb017ff550c5d
MarioDev716 <mariusz.sielnicki@gmail.com>
2022-11-10 21:26:03 +0100

    Roboczy

--- js/script.js
+++ js/script.js
@@ -97,6 +97,8 @@ function generateTitleLinks(customSelector = '') {
 generateTitleLinks();
 
 function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   // console.log('generateTags started!');
 
   /* [DONE] find all articles */
@@ -123,6 +125,11 @@ function generateTags() {
 
       /* add generated code to html variable */
       html = html + tagHTML;
+      /* [NEW] check if this link is NOT already in allTags */
+      if (allTags.indexOf(linkHTML) == -1) {
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     }
     /* END LOOP: for each tag */
 
@@ -130,6 +137,12 @@ function generateTags() {
     tagsWrapper.innerHTML = html;
   }
   /* END LOOP: for every article: */
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 
 generateTags();
@@ -229,56 +242,11 @@ function authorClickHandler(event) {
 }
 
 function addClickListenersToAuthors() {
-
   const authorLinks = document.querySelectorAll('.post-author a');
   console.log('authorLinks: ', authorLinks);
-  for (let author of authorLinks){
+  for (let author of authorLinks) {
     author.addEventListener('click', authorClickHandler);
   }
-  
-
 }
 
 addClickListenersToAuthors();
-
-function generateTags(){
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
-
-  /* find all articles */
-
-  /* START LOOP: for every article: */
-
-    /* find tags wrapper */
-
-    /* make html variable with empty string */
-
-    /* get tags from data-tags attribute */
-
-    /* split tags into array */
-
-    /* START LOOP: for each tag */
-
-      /* generate HTML of the link */
-
-      /* add generated code to html variable */
-
-      /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-        /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
-      }
-
-    /* END LOOP: for each tag */
-
-    /* insert HTML of all the links into the tags wrapper */
-
-  /* END LOOP: for every article: */
-
-  /* [NEW] find list of tags in right column */
-  const tagList = document.querySelector(optTagsListSelector);
-
-  /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
-}
-
